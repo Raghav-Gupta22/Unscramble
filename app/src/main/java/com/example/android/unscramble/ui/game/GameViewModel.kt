3c43cfdc4747644f6f551f81c0efd86fc5ebe799
Raghav-Gupta22 <raghavgupta216@gmail.com>
2021-08-03 23:56:36 +0530

    Updated getnextword() function

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -7,13 +7,36 @@ class GameViewModel : ViewModel() {
     init {
         Log.d("GameFragment", "GameViewModel created!")
     }
+
     private var score = 0
     private var currentWordCount = 0
     private var _currentScrambledWord = "test"
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
     val currentScrambledWord: String get() = _currentScrambledWord
 
     override fun onCleared() {
         super.onCleared()
         Log.d("GameFragment", "GameViewModel destroyed!")
+    }
+
+    /*
+* Updates currentWord and currentScrambledWord with the next word.
+*/
+    private fun getNextWord() {
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+        tempWord.shuffle()
+
+        while (tempWord.toString().equals(currentWord, false)) {
+            tempWord.shuffle()
+        }
+        if (wordsList.contains(currentWord)) {
+            getNextWord()
+        } else {
+            _currentScrambledWord = String(tempWord)
+            ++currentWordCount
+            wordsList.add(currentWord)
+        }
     }
 }
